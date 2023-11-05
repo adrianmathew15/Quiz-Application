0ae46a0e07f9726df5a1b3dc26be5c275b027a7e
Adrian Mathew <adrianaloysius02@gmail.com>
2023-11-05 22:38:01 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the full form of ALU?",
+    answer: "Arithmetic Logic Unit",
+    options: [
+      "Arithmetic Logic Unit",
+      "Arithmetic Local Unit",
+      "Advance Logical Unit",
+      "None of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Main memory of computer system is also called?",
+    answer: "Volatile",
+    options: [
+      "Non-Volatile",
+      "Large",
+      "Reserved",
+      "Volatile"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following memory of the computer is used to speed up the computer processing?",
+    answer: "Cache memory",
+    options: [
+      "Cache memory",
+      "RAM",
+      "ROM",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A hard disk is divided into tracks which are further subdivided into?",
+    answer: "Sectors",
+    options: [
+      "Clusters",
+      "Sectors",
+      "Vectors",
+      "Heads"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Multi-programming of computer system increases?",
+    answer: "CPU utilization",
+    options: [
+      "CPU utilization",
+      "Storage",
+      "Memory",
+      "Cost"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
